6a0cac4055a14b4863a37f433a68e84695ddbeb5
Uros <urosivanovich@gmail.com>
2022-03-01 17:25:02 -0500

    schema finshed

--- api/data/migrations/20220228190251_family_recipes.js
+++ api/data/migrations/20220228190251_family_recipes.js
@@ -1,14 +1,65 @@
 
 exports.up = async (knex) => {
     await knex.schema
-      .createTable('users', (users) => {
-        users.increments('user_id')
-        users.string('username', 200).notNullable()
-        users.string('password', 200).notNullable()
-        users.timestamps(false, true)
+      .createTable('users', tbl => {
+        tbl.increments('user_id')
+        tbl.string('username', 200).notNullable()
+        tbl.string('password', 200).notNullable()
+        tbl.timestamps(false, true)
+      })
+      .createTable('recipes', tbl => {
+        tbl.increments('recipe_id')
+        tbl.string('title', 200).notNullable()
+        tbl.string('source', 200)
+        tbl.string('category').notNullable()
+        tbl.integer('user_id')
+        .unsigned()
+        .notNullable()
+        .references('user_id')
+        .inTable('users')
+        .onDelete('CASCADE')
+        .onUpdate('CASCADE')
+      })
+      .createTable('ingredients', tbl => {
+        tbl.increments('ingredient_id')
+        tbl.string('ingredient_name').notNullable().unique()
+      })
+      .createTable('instructions', tbl => {
+        tbl.increments('instruction_id')
+        tbl.string('instruction_text', 200).notNullable()
+        tbl.integer('step_number').notNullable()
+        tbl.integer('recipe_id')
+        .unsigned()
+        .notNullable()
+        .references('recipe_id')
+        .inTable('recipes')
+        .onDelete('CASCADE')
+        .onUpdate('CASCADE')
+      })
+      .createTable('ing_instructions', tbl => {
+        tbl.increments('ing_instructions_id')
+        tbl.float('quantity').notNullable()
+        tbl.integer('ingredient_id')
+        .unsigned()
+        .notNullable()
+        .references('ingredient_id')
+        .inTable('ingredients')
+        .onDelete('CASCADE')
+        .onUpdate('CASCADE')
+        tbl.integer('instruction_id')
+        .unsigned()
+        .notNullable()
+        .references('instruction_id')
+        .inTable('instructions')
+        .onDelete('CASCADE')
+        .onUpdate('CASCADE')
       })
   }
   
   exports.down = async (knex) => {
+    await knex.schema.dropTableIfExists('ing_instructions')
+    await knex.schema.dropTableIfExists('instructions')
+    await knex.schema.dropTableIfExists('ingredients')
+    await knex.schema.dropTableIfExists('recipes')
     await knex.schema.dropTableIfExists('users')
   }

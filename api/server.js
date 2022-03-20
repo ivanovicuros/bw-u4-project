1b43979bd70fe5cb3668bb43686fa80512711e49
Uros <urosivanovich@gmail.com>
2022-03-20 15:31:10 -0400

    temporary endpoint

--- api/server.js
+++ api/server.js
@@ -4,15 +4,33 @@ const cors = require('cors')
 const RecipesRouter = require('./recipes/recipes-router')
 const AuthRouter = require('./auth/auth-router')
 const { restricted } = require('../api/auth/auth-middleware')
-
-
 const server = express()
+const axios = require('axios')
+
 server.use(express.json())
 server.use(helmet())
 server.use(cors())
 
+const API_KEY = process.env.API_KEY
+
 server.use('/api/auth', AuthRouter)
 server.use('/api/recipes', restricted, RecipesRouter)
+
+server.get('/weather', (req, res) => { // temporary endpoint for the purpose of small weather app
+    const location = req.query.q   
+    const options = {
+        method: 'GET',
+        url: 'https://api.openweathermap.org/data/2.5/weather',
+        params: {q: location, appid: API_KEY} 
+    }
+    axios.request(options)
+    .then(resp => {
+      res.json(resp.data)
+    }).catch(err => {
+        console.log(err)
+    })
+    
+})
 
 
 server.use('*', (req, res) => {
